e44a2c4b706846f1edd61f5ea03f6c314ea6bb98
Stefan Kraus <dev@stefankraus.org>
2024-12-03 20:19:19 +0000

    feat: add 2024 day03 part01
    Signed-off-by: Stefan Kraus <dev@stefankraus.org>

---
+++ aoc2024/src/bin/03.rs
@@ -0,0 +1,65 @@
+use adv_code_2024::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use regex::Regex;
+
+const DAY: &str = "03";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<i64> {
+        let answer = reader.lines();
+        let mut result = 0;
+        for line in answer {
+            let line = line?;
+            let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("No valid regex given?");
+            let res: i64 = regex
+                .captures_iter(line.as_str())
+                .map(|cap| {
+                    let a = cap[1].parse::<i64>().expect("Cannot parse number");
+                    let b = cap[2].parse::<i64>().expect("Cannot parse number");
+                    a * b
+                })
+                .sum();
+            result += res;
+        }
+
+        Ok(result)
+    }
+
+    assert_eq!(161, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    // println!("\n=== Part 2 ===");
+    //
+    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    //     Ok(0)
+    // }
+    //
+    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    //
+    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    // let result = time_snippet!(part2(input_file)?);
+    // println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
