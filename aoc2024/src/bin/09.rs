7191ab8233499bb4331e12bd275cac6443f521da
Stefan Kraus <dev@stefankraus.org>
2024-12-09 09:39:11 +0000

    feat: add 2024 day09 part02
    Signed-off-by: Stefan Kraus <dev@stefankraus.org>

--- aoc2024/src/bin/09.rs
+++ aoc2024/src/bin/09.rs
@@ -13,24 +13,35 @@ const TEST: &str = "\
 2333133121414131402
 ";
 
-pub struct UncompressIterator<'a> {
-    v: &'a Vec<i8>,
-    idx: i64,
-    cur_val: i8,
-    max_val: i8,
+fn read_input<R: BufRead>(mut reader: R) -> Vec<u8> {
+    let mut buffer = String::new();
+    let _ = reader.read_line(&mut buffer);
+    let input = buffer
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u8)
+        .collect_vec();
+    input
 }
 
-fn get_uncompress_iterator(vec: &Vec<i8>) -> UncompressIterator {
+pub struct UncompressIterator<'a> {
+    v: &'a Vec<u8>,
+    idx: i32,
+    cur_val: u8,
+    max_val: u8,
+}
+
+fn get_uncompress_iterator(vec: &Vec<u8>) -> UncompressIterator {
     UncompressIterator {
         v: vec,
         idx: -1,
-        cur_val: -1,
-        max_val: -1,
+        cur_val: 0,
+        max_val: 0,
     }
 }
 
 impl<'a> Iterator for UncompressIterator<'a> {
-    type Item = i64;
+    type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.cur_val < self.max_val {
@@ -39,7 +50,7 @@ impl<'a> Iterator for UncompressIterator<'a> {
         } else {
             self.cur_val = 0;
             self.idx += 1;
-            if self.idx >= self.v.len() as i64 {
+            if self.idx >= self.v.len() as i32 {
                 return None;
             }
             self.max_val = self.v[self.idx as usize];
@@ -48,31 +59,36 @@ impl<'a> Iterator for UncompressIterator<'a> {
     }
 }
 
+struct Hole {
+    position: u32,
+    size: u32,
+}
+
+struct BlockFile {
+    start_pos: u32,
+    size: u32,
+    idx: u32,
+}
+
 fn main() -> Result<()> {
     start_day(DAY);
 
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn part1<R: BufRead>(mut reader: R) -> Result<i64> {
-        let mut buffer = String::new();
-        let _ = reader.read_line(&mut buffer);
-        let input = buffer
-            .trim()
-            .chars()
-            .map(|c| c.to_digit(10).unwrap() as i8)
-            .collect_vec();
+    fn part1<R: BufRead>(reader: R) -> Result<i64> {
+        let input = read_input(reader);
         let front: Vec<_> = input.clone().into_iter().step_by(2).collect();
         let back: Vec<_> = input.clone().into_iter().rev().step_by(2).collect();
 
-        let num_blocks = front.iter().fold(0i64, |acc, e| acc + (*e as i64));
-        let max_block_id = (back.len() - 1) as i64;
+        let num_blocks = front.iter().fold(0i32, |acc, e| acc + (*e as i32));
+        let max_block_id = (back.len() - 1) as i32;
 
         let mut front = get_uncompress_iterator(&front);
         let mut back = get_uncompress_iterator(&back);
         let mut uncompressed = get_uncompress_iterator(&input);
 
-        let mut result = 0;
+        let mut result = 0i64;
         for i in 0..num_blocks {
             let num = uncompressed.next().unwrap();
             let block_id = if num % 2 == 0 {
@@ -80,7 +96,7 @@ fn main() -> Result<()> {
             } else {
                 max_block_id - back.next().unwrap()
             };
-            result += i * block_id;
+            result += (i * block_id) as i64;
         }
 
         Ok(result)
@@ -94,17 +110,57 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<u64> {
+        let input = read_input(reader);
+
+        let mut files = vec![];
+        let mut holes = vec![];
+        let mut pos = 0;
+        for (i, val) in input.into_iter().enumerate() {
+            let val = val as u32;
+            if i % 2 == 0 {
+                files.push(BlockFile {
+                    idx: i as u32 / 2,
+                    start_pos: pos,
+                    size: val,
+                });
+            } else {
+                holes.push(Hole {
+                    position: pos,
+                    size: val,
+                });
+            }
+            pos += val;
+        }
+
+        for blockfile in files.iter_mut().rev() {
+            let first_hole = holes
+                .iter_mut()
+                .find(|h| h.size >= blockfile.size && h.position < blockfile.start_pos);
+            if let Some(hole) = first_hole {
+                blockfile.start_pos = hole.position;
+                hole.size -= blockfile.size;
+                hole.position += blockfile.size;
+            };
+        }
+
+        let mut result = 0;
+        for block_file in files.iter() {
+            for i in 0..block_file.size {
+                result += ((block_file.start_pos + i) * block_file.idx) as u64;
+            }
+        }
+
+        Ok(result)
+    }
+
+    assert_eq!(2858, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
