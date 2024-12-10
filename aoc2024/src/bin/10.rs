e230ed3c0cc0e7a36c74cc43715070da39a204e0
Stefan Kraus <dev@stefankraus.org>
2024-12-10 08:59:51 +0000

    feat: add 2024 day10 part02
    Signed-off-by: Stefan Kraus <dev@stefankraus.org>

--- aoc2024/src/bin/10.rs
+++ aoc2024/src/bin/10.rs
@@ -2,9 +2,9 @@ use adv_code_2024::*;
 use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use itertools::Itertools;
 
 const DAY: &str = "10";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
@@ -22,35 +22,47 @@ const TEST: &str = "\
 
 const NEIGHBOURS: [[i32; 2]; 4] = [[-1, 0], [1, 0], [0, -1], [0, 1]];
 
-fn traverse(cur_pos: (i32, i32), field: &Vec<Vec<i32>>, expected_val: i32) -> HashSet<(i32, i32)> {
+fn read_input<R: BufRead>(reader: R) -> Vec<Vec<i32>>  {
+    let input = reader
+        .lines()
+        .map(|x| {
+            x.unwrap()
+                .trim()
+                .chars()
+                .map(|ch| ch.to_digit(10).unwrap() as i32)
+                .collect::<Vec<i32>>()
+        })
+        .collect::<Vec<Vec<i32>>>();
+    input
+}
+
+fn traverse(cur_pos: (i32, i32), field: &Vec<Vec<i32>>, expected_val: i32) -> Vec<(i32, i32)> {
     if cur_pos.0 < 0
         || cur_pos.1 < 0
         || cur_pos.0 >= field.len() as i32
         || cur_pos.1 >= field[0].len() as i32
     {
-        return HashSet::new();
+        return vec![];
     }
     if field[cur_pos.0 as usize][cur_pos.1 as usize] != expected_val {
-        return HashSet::new();
+        return vec![];
     }
 
     if expected_val == 9 {
-        let mut set = HashSet::new();
-        set.insert((cur_pos.0, cur_pos.1));
-        return set;
+        return vec![(cur_pos.0, cur_pos.1)];
     }
 
-    let mut set: HashSet<_> = HashSet::new();
+    let mut points = vec![];
     for n in NEIGHBOURS {
         let s = traverse(
             (cur_pos.0 + n[0], cur_pos.1 + n[1]),
             field,
             expected_val + 1,
         );
-        set = set.union(&s).copied().collect();
+        points.extend(s.iter());
     }
 
-    set
+    points
 }
 
 fn main() -> Result<()> {
@@ -58,27 +70,27 @@ fn main() -> Result<()> {
 
     //region Part 1
     println!("=== Part 1 ===");
+    
+    fn sol<R: BufRead>(reader: R, unique: bool) -> Result<usize>{
 
-    fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        let input = reader
-            .lines()
-            .map(|x| {
-                x.unwrap()
-                    .trim()
-                    .chars()
-                    .map(|ch| ch.to_digit(10).unwrap() as i32)
-                    .collect::<Vec<i32>>()
-            })
-            .collect::<Vec<Vec<i32>>>();
+        let input = read_input(reader);
 
         let mut res = 0;
         for (i, a) in input.iter().enumerate() {
             for j in 0..a.iter().len() {
-                res += traverse((i as i32, j as i32), &input, 0).len();
+                let mut traversal_result = traverse((i as i32, j as i32), &input, 0);
+                if unique {
+                    traversal_result = traversal_result.iter().unique().copied().collect();
+                }
+                res += traversal_result.len();
             }
         }
 
         Ok(res)
+    }
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        sol(reader, true)
     }
 
     assert_eq!(36, part1(BufReader::new(TEST.as_bytes()))?);
@@ -89,17 +101,17 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+    
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        sol(reader, false)
+    }
+    
+    assert_eq!(81, part2(BufReader::new(TEST.as_bytes()))?);
+    
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
