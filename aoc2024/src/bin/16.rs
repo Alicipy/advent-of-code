328accc4954d670eef155bc556361bbe83e02dd1
Stefan Kraus <dev@stefankraus.org>
2024-12-16 10:27:02 +0000

    feat: add 2024 day16 part02
    Signed-off-by: Stefan Kraus <dev@stefankraus.org>

--- aoc2024/src/bin/16.rs
+++ aoc2024/src/bin/16.rs
@@ -3,6 +3,7 @@ use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use priority_queue::PriorityQueue;
+use std::cmp::Reverse;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -56,10 +57,10 @@ fn parse_input<R: BufRead>(reader: R) -> Result<Vec<Vec<char>>> {
     Ok(labyrinth)
 }
 
-fn search_start_pos(labyrinth: &[Vec<char>]) -> (usize, usize) {
+fn search_in_lab(labyrinth: &[Vec<char>], needle: char) -> (usize, usize) {
     for (i, line) in labyrinth.iter().enumerate() {
         for (j, c) in line.iter().enumerate() {
-            if *c == 'S' {
+            if *c == needle {
                 return (i, j);
             }
         }
@@ -73,80 +74,118 @@ struct Reindeer {
     orientation: u8,
 }
 
-fn is_end_pos(labyrinth: &[Vec<char>], pos: &(usize, usize)) -> bool {
-    labyrinth[pos.0][pos.1] == 'E'
-}
-
 const MOVEMENT_VECTOR: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 
+fn traverse_labyrinth(
+    labyrinth: &[Vec<char>],
+    start_reindeer: Reindeer,
+    end_reindeer: Reindeer,
+) -> Option<u32> {
+    let mut pq = PriorityQueue::new();
+
+    // not the type one should be proud of.
+    let mut visited = vec![vec![vec![None::<u32>; 4]; labyrinth[0].len()]; labyrinth.len()];
+
+    pq.push(start_reindeer, Reverse(0));
+
+    while !pq.is_empty() {
+        let (reindeer, Reverse(prio)) = pq.pop().unwrap();
+        let Reindeer {
+            position,
+            orientation,
+        } = reindeer;
+
+        if reindeer == end_reindeer {
+            return Some(prio);
+        }
+
+        if labyrinth[position.0][position.1] == '#' {
+            continue;
+        }
+
+        if visited[position.0][position.1][orientation as usize].is_some() {
+            continue;
+        }
+
+        visited[position.0][position.1][orientation as usize] = Some(prio);
+
+        let dir = MOVEMENT_VECTOR[orientation as usize];
+
+        // turn left
+        pq.push_increase(
+            Reindeer {
+                position,
+                orientation: (orientation + 3) % 4,
+            },
+            Reverse(prio + 1000),
+        );
+        // turn right
+        pq.push_increase(
+            Reindeer {
+                position,
+                orientation: (orientation + 1) % 4,
+            },
+            Reverse(prio + 1000),
+        );
+        // step
+        pq.push_increase(
+            Reindeer {
+                position: (
+                    (position.0 as i32 + dir.0) as usize,
+                    (position.1 as i32 + dir.1) as usize,
+                ),
+                orientation,
+            },
+            Reverse(prio + 1),
+        );
+    }
+
+    None
+}
+
+fn construct_part1_min(
+    labyrinth: &[Vec<char>],
+    start_pos: (usize, usize),
+    end_pos: (usize, usize),
+) -> Result<u32> {
+    let start_reindeer = Reindeer {
+        position: start_pos,
+        orientation: 1,
+    };
+
+    let min_cost = (0..4)
+        .map(|i| {
+            traverse_labyrinth(
+                labyrinth,
+                start_reindeer,
+                Reindeer {
+                    position: end_pos,
+                    orientation: i,
+                },
+            )
+            .unwrap()
+        })
+        .min()
+        .unwrap();
+
+    Ok(min_cost)
+}
 fn main() -> Result<()> {
     start_day(DAY);
 
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn part1<R: BufRead>(reader: R) -> Result<i32> {
+    fn part1<R: BufRead>(reader: R) -> Result<u32> {
         let labyrinth = parse_input(reader)?;
 
-        let start_reindeer = Reindeer {
-            position: search_start_pos(&labyrinth),
-            orientation: 1,
-        };
+        let start_pos = search_in_lab(&labyrinth, 'S');
+        let end_pos = search_in_lab(&labyrinth, 'E');
 
-        let mut pq = PriorityQueue::new();
-        pq.push(start_reindeer, 0);
-
-        while !pq.is_empty() {
-            let (reindeer, prio) = pq.pop().unwrap();
-            let Reindeer {
-                position,
-                orientation,
-            } = reindeer;
-
-            if labyrinth[position.0][position.1] == '#' {
-                continue;
-            }
-
-            if is_end_pos(&labyrinth, &position) {
-                return Ok(-prio);
-            }
-
-            let dir = MOVEMENT_VECTOR[orientation as usize];
-
-            // turn left
-            pq.push_increase(
-                Reindeer {
-                    position,
-                    orientation: (orientation + 3) % 4,
-                },
-                prio - 1000,
-            );
-            // turn right
-            pq.push_increase(
-                Reindeer {
-                    position,
-                    orientation: (orientation + 1) % 4,
-                },
-                prio - 1000,
-            );
-            // step
-            pq.push_increase(
-                Reindeer {
-                    position: (
-                        (position.0 as i32 + dir.0) as usize,
-                        (position.1 as i32 + dir.1) as usize,
-                    ),
-                    orientation,
-                },
-                prio - 1,
-            );
-        }
-
-        panic!("No way found to to end?");
+        construct_part1_min(&labyrinth, start_pos, end_pos)
     }
 
     assert_eq!(7036, part1(BufReader::new(TEST1.as_bytes()))?);
-    assert_eq!(11048, part1(BufReader::new(TEST2.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
@@ -154,43 +193,69 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let labyrinth = parse_input(reader)?;
+
+        let start_pos = search_in_lab(&labyrinth, 'S');
+        let end_pos = search_in_lab(&labyrinth, 'E');
+
+        let start_reindeer = Reindeer {
+            position: start_pos,
+            orientation: 1,
+        };
+
+        let min_cost = construct_part1_min(&labyrinth, start_pos, end_pos)?;
+
+        let mut result_map = vec![vec![false; labyrinth[0].len()]; labyrinth.len()];
+
+        for (i, row) in labyrinth.iter().enumerate() {
+            for (j, c) in row.iter().enumerate() {
+                println!("{}, {}", i, j);
+                if *c == '#' {
+                    continue;
+                }
+                'tile_check: for k in 0..4 {
+                    let temp_reindeer_pos = Reindeer {
+                        position: (i, j),
+                        orientation: k,
+                    };
+
+                    let start_to_temp_shortest =
+                        traverse_labyrinth(&labyrinth, start_reindeer, temp_reindeer_pos).unwrap();
+
+                    for l in 0..4 {
+                        let end_reindeer_pos = Reindeer {
+                            position: end_pos,
+                            orientation: l,
+                        };
+
+                        let temp_to_end_shortest =
+                            traverse_labyrinth(&labyrinth, temp_reindeer_pos, end_reindeer_pos)
+                                .unwrap();
+
+                        if start_to_temp_shortest + temp_to_end_shortest == min_cost {
+                            result_map[i][j] = true;
+                            break 'tile_check;
+                        }
+                    }
+                }
+            }
+        }
+
+        let num_fields = result_map.iter().flatten().filter(|&&x| x).count();
+
+        Ok(num_fields)
+    }
+
+    assert_eq!(45, part2(BufReader::new(TEST1.as_bytes()))?);
+    assert_eq!(64, part2(BufReader::new(TEST2.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_pq_new_is_higher_get_new() {
-        let mut pq = priority_queue::PriorityQueue::new();
-        pq.push_increase(1, -5);
-        pq.push_increase(1, -4);
-
-        let elem = pq.pop().unwrap();
-        assert_eq!(elem.1, -4);
-    }
-
-    #[test]
-    fn test_pq_new_is_lower_get_old() {
-        let mut pq = priority_queue::PriorityQueue::new();
-        pq.push_increase(1, -5);
-        pq.push_increase(1, -6);
-
-        let elem = pq.pop().unwrap();
-        assert_eq!(elem.1, -5);
-    }
 }
