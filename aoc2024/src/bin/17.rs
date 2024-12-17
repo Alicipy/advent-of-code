6600cfe3b8f0123d4580a9c593bcf0962a6fa412
Stefan Kraus <dev@stefankraus.org>
2024-12-17 09:11:53 +0000

    feat: add 2024 day17 part01
    Signed-off-by: Stefan Kraus <dev@stefankraus.org>

---
+++ aoc2024/src/bin/17.rs
@@ -0,0 +1,214 @@
+use adv_code_2024::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "17";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST1: &str = "\
+Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0
+";
+
+struct ProgramState {
+    reg_a: u64,
+    reg_b: u64,
+    reg_c: u64,
+    pc: usize,
+}
+
+fn parse_input<R: BufRead>(reader: R) -> Result<(ProgramState, Vec<u8>)> {
+    let mut relevant_input_parts = vec![];
+    for line in reader.lines() {
+        let line = line?;
+        if line.is_empty() {
+            continue;
+        }
+        let part = line.split_once(": ").unwrap().1.to_string();
+        relevant_input_parts.push(part);
+    }
+    assert_eq!(relevant_input_parts.len(), 4);
+
+    let reg_a = relevant_input_parts[0].parse::<u64>()?;
+    let reg_b = relevant_input_parts[1].parse::<u64>()?;
+    let reg_c = relevant_input_parts[2].parse::<u64>()?;
+
+    let instructions = relevant_input_parts[3]
+        .split(",")
+        .map(|x| x.parse::<u8>().unwrap())
+        .collect();
+
+    let state = ProgramState {
+        reg_a,
+        reg_b,
+        reg_c,
+        pc: 0,
+    };
+
+    Ok((state, instructions))
+}
+
+fn get_combo_operand_value(combo_operand: u8, program_state: &ProgramState) -> u64 {
+    match combo_operand {
+        0..=3 => combo_operand as u64,
+        4 => program_state.reg_a,
+        5 => program_state.reg_b,
+        6 => program_state.reg_c,
+        _ => panic!("invalid combo operand '{}'", combo_operand),
+    }
+}
+
+fn simulate(state: ProgramState, instructions: &[u8]) -> Option<(ProgramState, Option<u8>)> {
+    let pc = state.pc;
+    if pc >= instructions.len() {
+        return None;
+    }
+    let opcode = instructions[pc];
+    let operand = instructions[pc + 1];
+
+    let mut output = None;
+
+    let new_state = match opcode {
+        0 => {
+            let numerator = state.reg_a;
+            let div_base_val = get_combo_operand_value(operand, &state);
+            let div_res = numerator / (2_u64.pow(div_base_val as u32));
+
+            ProgramState {
+                reg_a: div_res,
+                pc: pc + 2,
+                ..state
+            }
+        }
+        1 => {
+            let new_reg_b = state.reg_b ^ (operand as u64);
+            ProgramState {
+                reg_b: new_reg_b,
+                pc: pc + 2,
+                ..state
+            }
+        }
+        2 => {
+            let mod_val = get_combo_operand_value(operand, &state) % 8;
+            ProgramState {
+                reg_b: mod_val,
+                pc: pc + 2,
+                ..state
+            }
+        }
+        3 => ProgramState {
+            pc: if state.reg_a == 0 {
+                pc + 2
+            } else {
+                operand as usize
+            },
+            ..state
+        },
+        4 => ProgramState {
+            reg_b: state.reg_b ^ state.reg_c,
+            pc: pc + 2,
+            ..state
+        },
+        5 => {
+            let val = get_combo_operand_value(operand, &state) % 8;
+            output = Some(val as u8);
+
+            ProgramState {
+                pc: pc + 2,
+                ..state
+            }
+        }
+        6 => {
+            let numerator = state.reg_a;
+            let div_base_val = get_combo_operand_value(operand, &state);
+            let div_res = numerator / (2_u64.pow(div_base_val as u32));
+
+            ProgramState {
+                reg_b: div_res,
+                pc: pc + 2,
+                ..state
+            }
+        }
+        7 => {
+            let numerator = state.reg_a;
+            let div_base_val = get_combo_operand_value(operand, &state);
+            let div_res = numerator / (2_u64.pow(div_base_val as u32));
+
+            ProgramState {
+                reg_c: div_res,
+                pc: pc + 2,
+                ..state
+            }
+        }
+        _ => {
+            panic!("invalid opcode '{}'", opcode);
+        }
+    };
+
+    Some((new_state, output))
+}
+
+fn simulate_program(initial_program_state: ProgramState, instructions: &[u8]) -> Vec<u8> {
+    let mut current_program_state = initial_program_state;
+
+    let mut full_output = vec![];
+
+    while let Some((state, output)) = simulate(current_program_state, instructions) {
+        current_program_state = state;
+        if let Some(content) = output {
+            full_output.push(content);
+        }
+    }
+    full_output
+}
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<String> {
+        let (initial_program_state, instructions) = parse_input(reader)?;
+
+        let full_output = simulate_program(initial_program_state, &instructions);
+
+        Ok(full_output
+            .iter()
+            .map(u8::to_string)
+            .collect::<Vec<_>>()
+            .join(","))
+    }
+
+    assert_eq!(
+        "4,6,3,5,6,3,5,2,1,0",
+        part1(BufReader::new(TEST1.as_bytes()))?
+    );
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    // println!("\n=== Part 2 ===");
+    //
+    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    //     Ok(0)
+    // }
+    //
+    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    //
+    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    // let result = time_snippet!(part2(input_file)?);
+    // println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
