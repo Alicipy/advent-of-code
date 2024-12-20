a76e9b627ab7ba5b1de1e938497b8ddb1292b553
Stefan Kraus <dev@stefankraus.org>
2024-12-20 08:45:27 +0000

    feat: add 2024 day20 part02
    Signed-off-by: Stefan Kraus <dev@stefankraus.org>

--- aoc2024/src/bin/20.rs
+++ aoc2024/src/bin/20.rs
@@ -89,42 +89,51 @@ struct Cheat {
     saved_secs: u32,
 }
 
-fn calculate_cheats(distances: &[Vec<Option<u32>>]) -> Vec<Cheat> {
+fn calculate_cheats(distances: &[Vec<Option<u32>>], max_dist: u32) -> Vec<Cheat> {
     let mut cheats = vec![];
+
+    let signed_dist = max_dist as i32;
 
     for (xs, line) in distances.iter().enumerate() {
         for (ys, start_pos) in line.iter().enumerate() {
             match start_pos {
                 None => {}
                 Some(start_distance) => {
-                    for n in NEIGHBOURS {
-                        let xe = xs as i32 + 2 * n.0;
-                        let ye = ys as i32 + 2 * n.1;
-
-                        if xe < 0
-                            || ye < 0
-                            || xe as usize >= distances.len()
-                            || ye as usize >= distances[ys].len()
-                        {
-                            continue;
-                        }
-
-                        let xe = xe as usize;
-                        let ye = ye as usize;
-
-                        if let Some(end_distance) = distances[xe][ye] {
-                            let saved_secs = start_distance
-                                .saturating_sub(end_distance)
-                                .saturating_sub(2);
-                            if saved_secs == 0 {
+                    for xd in -signed_dist..=signed_dist {
+                        for yd in -signed_dist..=signed_dist {
+                            let cheat_time = (xd.abs() + yd.abs()) as u32;
+                            if cheat_time > max_dist {
                                 continue;
                             }
-                            let cheat = Cheat {
-                                _start_point: (xs, ys),
-                                _end_point: (xe, ye),
-                                saved_secs,
-                            };
-                            cheats.push(cheat);
+
+                            let xe = xs as i32 + xd;
+                            let ye = ys as i32 + yd;
+
+                            if xe < 0
+                                || ye < 0
+                                || xe as usize >= distances.len()
+                                || ye as usize >= distances[ys].len()
+                            {
+                                continue;
+                            }
+
+                            let xe = xe as usize;
+                            let ye = ye as usize;
+
+                            if let Some(end_distance) = distances[xe][ye] {
+                                let saved_secs = start_distance
+                                    .saturating_sub(end_distance)
+                                    .saturating_sub(cheat_time);
+                                if saved_secs == 0 {
+                                    continue;
+                                }
+                                let cheat = Cheat {
+                                    _start_point: (xs, ys),
+                                    _end_point: (xe, ye),
+                                    saved_secs,
+                                };
+                                cheats.push(cheat);
+                            }
                         }
                     }
                 }
@@ -146,7 +155,7 @@ fn main() -> Result<()> {
 
         let distances = calculate_dist_to_end(&labyrinth);
 
-        let cheats = calculate_cheats(&distances);
+        let cheats = calculate_cheats(&distances, 2);
 
         Ok(cheats
             .into_iter()
@@ -165,17 +174,27 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R, minimal_save_dist: u32) -> Result<u64> {
+        let labyrinth = parse_input(reader);
+
+        let distances = calculate_dist_to_end(&labyrinth);
+
+        let cheats = calculate_cheats(&distances, 20);
+
+        Ok(cheats
+            .into_iter()
+            .filter(|c| c.saved_secs >= minimal_save_dist)
+            .count() as u64)
+    }
+
+    assert_eq!(29, part2(BufReader::new(TEST1.as_bytes()), 72)?);
+    assert_eq!(3, part2(BufReader::new(TEST1.as_bytes()), 76)?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file, 100)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
