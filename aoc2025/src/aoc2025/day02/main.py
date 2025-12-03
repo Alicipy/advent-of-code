6e5d800d7dbd4990bb960bd4493b3cd2b0da97fc
Stefan Kraus <dev@stefankraus.org>
2025-12-03 19:38:04 +0100

    feat: add solution for day 2

---
+++ aoc2025/src/aoc2025/day02/main.py
@@ -0,0 +1,40 @@
+import itertools
+from typing import Callable
+
+
+def solve(lines: list[str], is_invalid_id: Callable[[str], bool]):
+    line = lines[0]
+
+    result = 0
+
+    for part in line.split(","):
+        start, end = part.split("-")
+
+        for i in range(int(start), int(end) + 1):
+            invalid = is_invalid_id(str(i))
+            if invalid:
+                result += i
+
+    return result
+
+
+def part_1(lines: list[str]) -> int | str:
+    return solve(lines, is_invalid_id_1)
+
+
+def part_2(lines: list[str]) -> int | str:
+    return solve(lines, is_invalid_id_2)
+
+
+def is_invalid_id_1(id: str) -> bool:
+    half_len = len(id) // 2
+    return id[half_len:] in id[:half_len]
+
+
+def is_invalid_id_2(id: str) -> bool:
+    for i in range(1, len(id) // 2 + 1):
+        groups_of_len = itertools.batched(id, i)
+        if len(set(groups_of_len)) == 1:
+            return True
+
+    return False
--- aoc2025/tests/test_day02.py
+++ aoc2025/tests/test_day02.py
@@ -1,9 +1,32 @@
 import pytest
 
-from aoc2025.day02.main import is_invalid_id
+from aoc2025.day02.main import part_1, part_2, is_invalid_id_1, is_invalid_id_2
+
+TEST_INPUT = (
+    "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,"
+    "1698522-1698528,446443-446449,38593856-38593862,"
+    "565653-565659,824824821-824824827,2121212118-2121212124"
+)
 
 
-@pytest.mark.parametrize("id", ["11", "22"])
-def test_is_invalid_id__several_invalid_ids__detected_invalid(invalid_id):
+def test__part_1__example():
+    assert part_1([TEST_INPUT]) == 1227775554
 
-    assert is_invalid_id(invalid_id)
+
+def test__part_2__example():
+    assert part_2([TEST_INPUT]) == 4174379265
+
+
+@pytest.mark.parametrize("invalid_id", ["11", "22", "1010"])
+def test_is_invalid_id_1_several_invalid_ids__detected_invalid(invalid_id):
+    assert is_invalid_id_1(invalid_id)
+
+
+@pytest.mark.parametrize("valid_id", ["12", "13", "1698522"])
+def test_is_invalid_id_1_no_invalid_ids__detected_valid(valid_id):
+    assert is_invalid_id_1(valid_id) == False
+
+
+@pytest.mark.parametrize("invalid_id", ["38593859"])
+def test_is_invalid_id_2_invalid_id__detected_invalid(invalid_id):
+    assert is_invalid_id_2(invalid_id)
